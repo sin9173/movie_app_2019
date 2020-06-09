a645a8686553896898948b1902914d7014ef4ce8
sin9173 <sid9173@gmail.com>
2020-06-09 14:39:21 +0900

    Updates

---
+++ precache-manifest.b26700821dc72cf2a4fed5e80eb4a2c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad5299e0d165d92210b21679cf7927cc",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "5ccb9d926200fb46bdfe",
+    "url": "/movie_app_2019/static/css/main.9c43b365.chunk.css"
+  },
+  {
+    "revision": "7676ec8dfa3a757764b3",
+    "url": "/movie_app_2019/static/js/2.cba50508.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.cba50508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ccb9d926200fb46bdfe",
+    "url": "/movie_app_2019/static/js/main.64602bc1.chunk.js"
+  },
+  {
+    "revision": "f5b00c7a31981ed629ee",
+    "url": "/movie_app_2019/static/js/runtime-main.140b3552.js"
+  }
+]);
